7080f60915cc4ae5d7bebf6cb76d7d5b6841e4e0
Jxthel <pjathel@gmail.com>
2025-09-18 23:41:08 -0600

    Update mobile DPM.js

--- index js/mobile DPM.js
+++ index js/mobile DPM.js
@@ -1,7 +1,8 @@
 //======JS para el menú hamburguesa de la navbar==============
-const toggleMenu = document.getElementById(".menu-toggle");
+const toggleMenu = document.querySelector(".menu-toggle");
 const enlacesNav = document.querySelector(".nav-links");
 
 toggleMenu.addEventListener("click", () => {
-  enlacesNav.classList.toggle("active");
+enlacesNav.classList.toggle("active");
 });
+
